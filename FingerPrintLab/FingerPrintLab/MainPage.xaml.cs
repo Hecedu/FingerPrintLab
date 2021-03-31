12ac4da5083a00540e45d6d1ad03a56396db4b83
Hecedu <hector.maganahdez@gmail.com>
2021-03-31 12:00:19 -0600

    Add project files.

---
+++ FingerPrintLab/FingerPrintLab/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FingerPrintLab
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void AuthButton_Clicked(object sender, EventArgs e)
+        {
+            bool isFingerprintAvailable = await CrossFingerprint.Current.IsAvailableAsync(false);
+            if (!isFingerprintAvailable)
+            {
+                await DisplayAlert("Error",
+                    "Biometric authentication is not available or is not configured.", "OK");
+                return;
+            }
+
+            AuthenticationRequestConfiguration conf =
+                new AuthenticationRequestConfiguration("Authentication",
+                "Authenticate access to your personal data");
+
+            var authResult = await CrossFingerprint.Current.AuthenticateAsync(conf);
+            if (authResult.Authenticated)
+            {
+                //Success  
+                await DisplayAlert("Success", "Authentication succeeded", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "Authentication failed", "OK");
+            }
+        }
+    }
+}
